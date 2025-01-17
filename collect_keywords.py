e98a24cf7b5d6584e619e20287885959d776ae21
yedam <dam04098@gmail.com>
2025-01-17 15:36:57 +0900

    test app update

--- collect_keywords.py
+++ collect_keywords.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import time
