dab7bc2f726203fa40683c1d2faa88d9448e5dd9
Seo-yedam <dam04098@gmail.com>
2025-01-22 16:51:10 +0900

    streamlit cloud 배포

--- visualization_cloud.py
+++ visualization_cloud.py
@@ -13,21 +13,6 @@ def get_crawler():
     from collect_keywords_cloud import NaverShoppingCrawler  # 함수 내부에서 import
     return NaverShoppingCrawler()
 
-# 폰트를 저장할 Streamlit 전용 디렉토리 설정
-font_dir = os.path.join(os.getcwd(), ".streamlit/fonts")
-font_path = os.path.join(font_dir, "NanumGothic.ttf")
-
-# 폰트가 없으면 다운로드
-if not os.path.exists(font_path):
-    os.makedirs(font_dir, exist_ok=True)  # 앱 디렉토리에 저장
-    font_url = "https://github.com/naver/nanumfont/releases/latest/download/NanumGothic.ttf"
-    urllib.request.urlretrieve(font_url, font_path)
-
-# Matplotlib에 폰트 추가 및 설정
-fm.fontManager.addfont(font_path)
-mpl.rc("font", family="NanumGothic")
-mpl.rcParams["axes.unicode_minus"] = False  # 마이너스(-) 기호 깨짐 방지
-
 # Streamlit UI 설정
 st.title("스마트스토어 상품명 키워드 분석")
 st.write("내 상품이 상위노출되려면, 어떤 키워드를 써야할까?")
@@ -58,15 +43,3 @@ if "sorted_scores" in st.session_state:
         score_df = pd.DataFrame(sorted_scores)
         score_df.index = [f"{i+1}위" for i in range(len(score_df))]
         st.table(score_df)
-        
-        # 그래프 시각화 (막대 그래프)
-        fig, ax = plt.subplots()
-        keywords = score_df["키워드"]
-        total_scores = score_df["총점"]
-
-        ax.barh(keywords[::-1], total_scores[::-1], color='skyblue')
-        ax.set_xlabel("총점")
-        ax.set_ylabel("키워드")
-        ax.set_title("추천 키워드 점수 분석")
-
-        st.pyplot(fig)
